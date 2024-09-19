d18ee060cd54ae416394716bac6f9d935fcbe987
Kallol Khatua <kallolkhatua2005@gmail.com>
2024-09-19 18:52:41 +0530

    add functionality to campaign calculator

--- client/src/components/Advertiser/CampaignCalculatorComponent.jsx
+++ client/src/components/Advertiser/CampaignCalculatorComponent.jsx
@@ -16,12 +16,75 @@ import { useSearchParams, Link as RouterLink } from "react-router-dom";
 
 export default function CampaignCalculatorComponent() {
   const [searchParams, setSearchParams] = useSearchParams();
-  const [campaignBudget, setCampaignBudget] = useState(500);
-  const [ctr, setCTR] = useState(0.0);
-  const [adImpression, setAdImpression] = useState(6000);
-  const [estimatedPayout, setEstimatedPayout] = useState(6);
+  const [campaignBudget, setCampaignBudget] = useState(1000);
+  const [ctr, setCTR] = useState(0.12);
+  const [cpm, setCPM] = useState(3.0);
   const [region, setRegion] = useState("network");
   const [topic, setTopic] = useState("all-developers");
+  const [expectedImpression, setExpectedImpression] = useState(333333);
+  const [expectedClicks, setExpectedClicks] = useState(1000);
+
+  const combinationData = [
+    { region: "network", topic: "security", amount: 4.33 },
+    { region: "network", topic: "datascience", amount: 4.25 },
+    { region: "network", topic: "devops", amount: 4.0 },
+    { region: "network", topic: "frontend", amount: 4.1 },
+    { region: "network", topic: "backend", amount: 3.6 },
+    { region: "network", topic: "all-developers", amount: 3.0 },
+
+    { region: "eng", topic: "security", amount: 6.75 },
+    { region: "eng", topic: "datascience", amount: 6.5 },
+    { region: "eng", topic: "devops", amount: 6.25 },
+    { region: "eng", topic: "frontend", amount: 6.5 },
+    { region: "eng", topic: "backend", amount: 5.75 },
+    { region: "eng", topic: "all-developers", amount: 5.0 },
+
+    { region: "blend", topic: "security", amount: 5.5 },
+    { region: "blend", topic: "datascience", amount: 5.37 },
+    { region: "blend", topic: "devops", amount: 5.12 },
+    { region: "blend", topic: "frontend", amount: 5.25 },
+    { region: "blend", topic: "backend", amount: 4.62 },
+    { region: "blend", topic: "all-developers", amount: 4.0 },
+
+    { region: "weu", topic: "security", amount: 4.25 },
+    { region: "weu", topic: "datascience", amount: 4.25 },
+    { region: "weu", topic: "devops", amount: 4.0 },
+    { region: "weu", topic: "frontend", amount: 4.0 },
+    { region: "weu", topic: "backend", amount: 3.5 },
+    { region: "weu", topic: "all-developers", amount: 3.0 },
+
+    { region: "eeu-apac", topic: "security", amount: 2.0 },
+    { region: "eeu-apac", topic: "datascience", amount: 2.0 },
+    { region: "eeu-apac", topic: "devops", amount: 1.75 },
+    { region: "eeu-apac", topic: "frontend", amount: 1.75 },
+    { region: "eeu-apac", topic: "backend", amount: 1.5 },
+    { region: "eeu-apac", topic: "all-developers", amount: 1.1 },
+
+    { region: "global", topic: "security", amount: 0.7 },
+    { region: "global", topic: "datascience", amount: 0.8 },
+    { region: "global", topic: "devops", amount: 0.7 },
+    { region: "global", topic: "frontend", amount: 0.7 },
+    { region: "global", topic: "backend", amount: 0.6 },
+    { region: "global", topic: "all-developers", amount: 0.5 },
+  ];
+
+  const regionFullName = {
+    network: "Run of Network",
+    eng: "US, Canada, UK, Australia, New Zealand, Ireland",
+    blend: "Blend: W. Europe, US, Canada, UK, Australia, New Zealand",
+    weu: "Western Europe",
+    "eeu-apac": "Eastern EU, Asia Pacific",
+    global: "Global",
+  };
+
+  const topicFullName = {
+    security: "Security & privacy",
+    datascience: "AI & machine learning",
+    devops: "DevOps",
+    frontend: "Frontend web development",
+    backend: "Backend web development",
+    "all-developers": "All developers",
+  };
 
   const handleChange = (event, newValue) => {
     setCampaignBudget(newValue);
@@ -63,17 +126,6 @@ export default function CampaignCalculatorComponent() {
     }
   }, [searchParams]);
 
-  useEffect(() => {
-    let impression = Math.round(campaignBudget * 0.3);
-    setAdImpression(impression);
-  }, [campaignBudget]);
-
-  useEffect(() => {
-    let payout = (adImpression / 1000) * ctr;
-    let roundOff = Math.round(payout * 100) / 100;
-    setEstimatedPayout(roundOff);
-  }, [adImpression, ctr]);
-
   const handleRegionChange = (event) => {
     setRegion(event.target.value);
     setSearchParams({
@@ -93,6 +145,30 @@ export default function CampaignCalculatorComponent() {
       ctr: ctr,
     });
   };
+
+  // check cpm on changing region or topic
+  useEffect(() => {
+    combinationData.forEach((data) => {
+      if (data.topic === topic && data.region === region) {
+        // console.log(data);
+        setCPM(Number(data.amount));
+      }
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [region, topic]);
+
+  // calculating number of impression and clicks on changing budget, ctr or cpm
+  useEffect(() => {
+    let impression = (1000 / cpm) * campaignBudget;
+    let roundOffImpression = Math.round(impression);
+    setExpectedImpression(roundOffImpression);
+  }, [campaignBudget, cpm]);
+
+  useEffect(() => {
+    let clicks = expectedImpression * ctr / 100;
+    let roundOffClicks = Math.round(clicks);
+    setExpectedClicks(roundOffClicks);
+  }, [expectedImpression, ctr]);
 
   return (
     <Box
@@ -166,13 +242,21 @@ export default function CampaignCalculatorComponent() {
                       value="eng"
                       control={<Radio />}
                       label="US, Canada, UK, Australia, New Zealand, Ireland"
-                      sx={{ marginBottom: -1.5 }}
+                      sx={{
+                        sm: {
+                          marginBottom: -1.5,
+                        },
+                      }}
                     />
                     <FormControlLabel
                       value="blend"
                       control={<Radio />}
                       label="Blend: W. Europe, US, Canada, UK, Australia, New Zealand"
-                      sx={{ marginBottom: -1.5 }}
+                      sx={{
+                        sm: {
+                          marginBottom: -1.5,
+                        },
+                      }}
                     />
                     <FormControlLabel
                       value="weu"
@@ -316,25 +400,44 @@ export default function CampaignCalculatorComponent() {
                   backgroundColor: "#506690",
                   color: "white",
                   borderRadius: "0.5rem",
+                  px: 2,
+                  textAlign: "center",
                 }}
               >
-                <p>Region</p>
-                <p>Run of Network</p>
+                <Typography>Region</Typography>
+                <Typography variant="h6" sx={{ mb: 2 }}>
+                  {regionFullName[region]}
+                </Typography>
 
-                <p>Topic</p>
-                <p>All developers</p>
+                <Typography>Topic</Typography>
+                <Typography variant="h6" sx={{ mb: 2 }}>
+                  {topicFullName[topic]}
+                </Typography>
 
-                <p>Budget</p>
-                <p>$1,000</p>
+                <Typography>Budget</Typography>
+                <Typography variant="h6" sx={{ mb: 2 }}>
+                  {campaignBudget}
+                </Typography>
 
-                <p>CPM</p>
-                <p>$2.75</p>
+                <Typography>CTR</Typography>
+                <Typography variant="h6" sx={{ mb: 2 }}>
+                  {ctr.toFixed(2)}%
+                </Typography>
 
-                <p>Number of impressions</p>
-                <p>363,636</p>
+                <Typography>CPM</Typography>
+                <Typography variant="h6" sx={{ mb: 2 }}>
+                  ${cpm.toFixed(2)}
+                </Typography>
 
-                <p>Number of clicks</p>
-                <p>436</p>
+                <Typography>Number of impressions</Typography>
+                <Typography variant="h6" sx={{ mb: 2 }}>
+                  {expectedImpression.toLocaleString()}
+                </Typography>
+
+                <Typography>Number of clicks</Typography>
+                <Typography variant="h6" sx={{ mb: 2 }}>
+                  {expectedClicks.toLocaleString()}
+                </Typography>
               </Box>
               <Button
                 color="primary"
